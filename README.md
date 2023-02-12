566746b13e204cf8cdffb8b6bac4ee406da05aca
Oluwatobi <116016360+Thobbie30@users.noreply.github.com>
2023-02-12 13:43:14 +0100

    My assignment

---
+++ README.md
@@ -0,0 +1,18 @@
+1. Node
+
+2. 5 things javascript does
+a) Carrying out arithmetic operations
+b) Game Development
+c) Building Websites
+d) Writing statements/sentences
+e) Artificial Intelligence
+
+3. June 1997
+
+4.  Go to your browser, click on inspect, click on console; then you can carry out any operation
+
+5. Frontend Development, Backend Development, Full stack Development, 
+
+6. Statistically typed language perform type checking at compile time. Examples are Java, C, C++ etc, while Dynamically typed language perform type checking at run time. Examples are Python, JavaScript, PHP, Ruby, Perl etc.
+
+7. Null: It is the intentional absence of the value. it is one of the primitive values of JavaScript. Undefined means the value does not exist in the compiler, it is a variable that has not been assigned a value it is the global object.
---
+++ index.js
@@ -0,0 +1,27 @@
+// Function that multiples two numbers
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+// Function to multiply my chosen two numbers
+console.log(multiply(20, 15));
+
+// Function that creates variables about me
+let Firstname = "Oluwatobi";
+let Lastname = "Adeosin";
+let Stuterntrack = "Cohort 1.4";
+let Skills = "Frontend Development";
+let Favourite_colour = "Blue";
+let Year_of_independence = "1960";
+
+/* Function to log these variables
+console.log(Firstname);
+console.log(Lastname);
+console.log(Stuterntrack);
+console.log(Skills);
+console.log(Favourite_colour);
+console.log(Year_of_independence);
+*/
+
+// Function to use these variables to form a sentence and log the sentence in the console
+console.log("My name is " + Firstname + ", My surname is " + Lastname + ", My track is " + Stuterntrack + ", I am currently learning " + Skills + ", My best_colour is " + Favourite_colour + ", My country's year of independence is " + Year_of_independence);
